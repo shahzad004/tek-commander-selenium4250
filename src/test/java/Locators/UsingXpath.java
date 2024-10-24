cf2563688bdcbe4348f7a98c5e60265c5b8b48cc
Shahzad <shahzadkhan36038@gmail.com>
2024-10-23 18:13:24 -0700

    ........

--- src/test/java/Locators/UsingXpath.java
+++ src/test/java/Locators/UsingXpath.java
@@ -10,13 +10,14 @@ public class UsingXpath {
         ChromeDriver driver= new ChromeDriver();
         driver.get("https://retail.tekschool-students.com/");
         driver.manage().window().maximize();
-
+        Thread.sleep(1500);
         driver.findElement(By.xpath("/html/body/div/div/div[1]/div[3]/div/div[2]/div/div[1]/div/div/div[2]/p")).click();
 
-       WebElement ptext = driver.findElement(By.xpath("/html/body/div/div/div/div[3]/div/div[1]/p"));
-        Thread.sleep(2000);
-       System.out.println(ptext.getText());
+       WebElement pElement = driver.findElement(By.xpath("/html/body/div/div/div/div[3]/div/div[1]/p"));
+        Thread.sleep(1000);
 
+       System.out.println(pElement.getText());
+        Thread.sleep(1000);
        driver.quit();
 
 
