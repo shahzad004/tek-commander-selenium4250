b212389a34894cc045ec930dbb82607cd3858492
Shahzad <shahzadkhan36038@gmail.com>
2024-10-24 16:35:06 -0700

    ...........

--- src/test/java/Locators/LabActivitOne.java
+++ src/test/java/Locators/LabActivitOne.java
@@ -3,25 +3,44 @@ package Locators;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import java.util.Scanner;
+
 public class LabActivitOne {
 
     public static void main(String[] args) throws InterruptedException {
 
-        ChromeDriver  driver=new ChromeDriver();
-        driver.get("https://dev.retail.tekschool-students.com/");
-        driver.manage().window().maximize();
+        Scanner input=new Scanner(System.in);
+        System.out.println("Which Browser you want to open?");
+        System.out.println("1: Chrome");
+        System.out.println("2: Edge");
 
-        String title= driver.getTitle();
-        System.out.println(title);
-        Thread.sleep(1000);
-        driver.quit();
 
-        EdgeDriver edgedriver=new EdgeDriver();
-        edgedriver.get("https://dev.retail.tekschool-students.com/");
-        edgedriver.manage().window().maximize();
-        Thread.sleep(1000);
-        String title2= edgedriver.getTitle();
-        System.out.println(title2);
+        int choice=input.nextInt()
+;       if(choice==1) {
+            ChromeDriver  driver=new ChromeDriver();
+            driver.get("https://dev.retail.tekschool-students.com/");
+            driver.manage().window().maximize();
+
+            String title= driver.getTitle();
+            System.out.println(title);
+            Thread.sleep(1000);
+            driver.quit();
+
+        }
+        if(choice==2){
+            EdgeDriver edgedriver=new EdgeDriver();
+            edgedriver.get("https://dev.retail.tekschool-students.com/");
+            edgedriver.manage().window().maximize();
+            Thread.sleep(1000);
+            String title2= edgedriver.getTitle();
+            System.out.println(title2);
+
+            edgedriver.quit();
+
+        }
+
+
+
 
 
 
