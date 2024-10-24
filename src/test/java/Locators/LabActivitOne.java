312212c72279c37a99eefd240ba66f45c67ec1b8
Shahzad <shahzadkhan36038@gmail.com>
2024-10-24 16:42:50 -0700

    ............

--- src/test/java/Locators/LabActivitOne.java
+++ src/test/java/Locators/LabActivitOne.java
@@ -1,5 +1,7 @@
 package Locators;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -15,31 +17,24 @@ public class LabActivitOne {
         System.out.println("2: Edge");
 
 
+        WebDriver driver=null;
+
         int choice=input.nextInt()
 ;       if(choice==1) {
-            ChromeDriver  driver=new ChromeDriver();
-            driver.get("https://dev.retail.tekschool-students.com/");
-            driver.manage().window().maximize();
-
-            String title= driver.getTitle();
-            System.out.println(title);
-            Thread.sleep(1000);
-            driver.quit();
-
-        }
-        if(choice==2){
-            EdgeDriver edgedriver=new EdgeDriver();
-            edgedriver.get("https://dev.retail.tekschool-students.com/");
-            edgedriver.manage().window().maximize();
-            Thread.sleep(1000);
-            String title2= edgedriver.getTitle();
-            System.out.println(title2);
-
-            edgedriver.quit();
-
-        }
+            driver= new ChromeDriver();}
+         if(choice==2){
+        driver=new EdgeDriver();}
 
 
+
+
+        driver.get("https://dev.retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        String title= driver.getTitle();
+        System.out.println(title);
+        Thread.sleep(1000);
+        driver.quit();
 
 
 
