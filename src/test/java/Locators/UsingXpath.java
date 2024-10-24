c476eaf74f71f30275d63b7fb6ec129ca2962f9a
Shahzad <shahzadkhan36038@gmail.com>
2024-10-23 18:28:38 -0700

    ..........

--- src/test/java/Locators/UsingXpath.java
+++ src/test/java/Locators/UsingXpath.java
@@ -12,8 +12,10 @@ public class UsingXpath {
         driver.manage().window().maximize();
         Thread.sleep(1500);
         driver.findElement(By.xpath("/html/body/div/div/div[1]/div[3]/div/div[2]/div/div[1]/div/div/div[2]/p")).click();
+        Thread.sleep(1500);
 
         driver.findElement(By.xpath("/html/body/div/div/div/div[3]/div/div[1]/p[1]")).click();
+        Thread.sleep(1000);
        WebElement pElement= driver.findElement(By.xpath("/html/body/div/div/div[1]/div[3]/div/div[1]/div[2]/h1"));
 
 
