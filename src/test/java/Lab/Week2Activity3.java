5ff07bcc40a72ac3a8d9b49725bcf67a84630fa7
Shahzad <shahzadkhan36038@gmail.com>
2024-10-28 18:37:52 -0700

    ....

---
+++ src/test/java/Lab/Week2Activity3.java
@@ -0,0 +1,31 @@
+package Lab;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Week2Activity3 {
+    public static void main(String[] args) throws InterruptedException {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("//a[@id='signinLink']"));
+
+        driver.findElement(By.xpath("//a[text()='Create New Account']")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("//button[@id='signupBtn'']")).click();
+
+   String nametext=     driver.findElement(By.id("//div[@id='nameError']")).getText();
+   Thread.sleep(1000);
+        String emailtext=     driver.findElement(By.id( " //div[@id='emailError']")).getText();
+        String passwordtext=     driver.findElement(By.id( " //div[@id='passwordError']")).getText();
+        String confirmpasstext=     driver.findElement(By.id( " //div[@id='confirmPasswordError']")).getText();
+
+        System.out.println(nametext+" "+ emailtext +" "+ passwordtext +" "+ confirmpasstext);
+
+        driver.quit();
+
+    }
+}
