c75d5493b440c0f310dc4b0bf479ddac91f48f3d
Shahzad <shahzadkhan36038@gmail.com>
2024-11-14 16:23:41 -0800

    comment

--- src/test/java/Week4/day3/Exercise.java
+++ src/test/java/Week4/day3/Exercise.java
@@ -11,7 +11,7 @@ import java.time.Duration;
 import java.util.Random;
 
 public class Exercise extends WebDriverSetup {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String urL="https://www.amazon.com/";
 
@@ -31,12 +31,13 @@ public class Exercise extends WebDriverSetup {
 
          WebElement buttonclick=driver.findElement(By.xpath("(//span[@class='nav-action-inner'][normalize-space()='Sign in'])[1]"));
         act.click(buttonclick).perform();
-
+        Thread.sleep(1000);
 
         wait.until(ExpectedConditions.urlToBe("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"));
 
 
-
+        closeTab();
+        quitBrower();
 
 
     }
